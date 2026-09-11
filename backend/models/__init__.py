1d37687e280a18cdf18f4aad815f69049b8e01f8
cbbeirne-create <contentmgtppl@gmail.com>
2026-09-11 11:35:20 +0100

    models: remove obsolete scan update export and expose auth models

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,22 +1,39 @@
-# Models module
+"""Public Pydantic model exports."""
 from .user import (
-    User, UserCreate, UserLogin, UserProfile,
-    Token, TokenData,
-    UserPlan, SubscriptionStatus,
-    ForgotPasswordRequest, ResetPasswordRequest, PasswordResetResponse
+    User,
+    UserCreate,
+    UserLogin,
+    UserProfile,
+    Token,
+    TokenData,
+    UserPlan,
+    SubscriptionStatus,
+    ForgotPasswordRequest,
+    ResetPasswordRequest,
+    PasswordResetResponse,
+    ResendVerificationRequest,
+    VerifyEmailRequest,
+    EmailVerificationResponse,
 )
-from .scan import (
-    ScanRequest, ScanRequestCreate, ScanRequestUpdate,
-    ScanStatus, ScanTool
-)
+from .scan import ScanRequest, ScanRequestCreate, ScanStatus, ScanTool
 
 __all__ = [
-    # User models
-    "User", "UserCreate", "UserLogin", "UserProfile",
-    "Token", "TokenData",
-    "UserPlan", "SubscriptionStatus",
-    "ForgotPasswordRequest", "ResetPasswordRequest", "PasswordResetResponse",
-    # Scan models
-    "ScanRequest", "ScanRequestCreate", "ScanRequestUpdate",
-    "ScanStatus", "ScanTool"
+    "User",
+    "UserCreate",
+    "UserLogin",
+    "UserProfile",
+    "Token",
+    "TokenData",
+    "UserPlan",
+    "SubscriptionStatus",
+    "ForgotPasswordRequest",
+    "ResetPasswordRequest",
+    "PasswordResetResponse",
+    "ResendVerificationRequest",
+    "VerifyEmailRequest",
+    "EmailVerificationResponse",
+    "ScanRequest",
+    "ScanRequestCreate",
+    "ScanStatus",
+    "ScanTool",
 ]
